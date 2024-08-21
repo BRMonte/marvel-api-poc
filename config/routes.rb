7cc25d04a79b03f3bef35c9830f3e9cd2494e685
Bruno Montebello <brunormontebello@hotmail.com>
2024-08-21 14:47:10 -0300

    Refact route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  patch 'comics/:id', to: 'comics#favorite', as: :favorite_comics
-
   root to: "comics#index"
-  # get 'comics/characters/:name', to: 'comics#characters', as: 'characters'
+  patch 'comics/:id', to: 'comics#favorite', as: :favorite_comics
   get 'comics/characters', to: 'comics#characters', as: 'characters'
 end
