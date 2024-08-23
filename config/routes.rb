208d7af814ec4e2914a6f2e306d0a73de03a32d9
Bruno Montebello <brunormontebello@hotmail.com>
2024-08-23 14:52:18 -0300

    Create characters own controller + remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  root to: "comics#index"
-  patch 'comics/:id', to: 'comics#favorite', as: :favorite_comics
-  get 'comics/characters', to: 'comics#characters', as: 'characters'
+  root 'pages#home'
+
+  # get 'comics', to: 'comics#index'
+  # patch 'comics/:id', to: 'comics#favorite', as: :favorite_comics
+  resources :comics, only: [:index] do
+    member do
+      patch 'favorite'
+    end
+  end
+
+  resources :characters do
+    collection do
+      get 'search'
+    end
+  end
 end
