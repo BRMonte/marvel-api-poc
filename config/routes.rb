c025068482354535d237fecaf46c1edb4f7c8876
Bruno Montebello <brunormontebello@hotmail.com>
2024-08-23 15:22:08 -0300

    fix comics spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  # get 'comics', to: 'comics#index'
-  # patch 'comics/:id', to: 'comics#favorite', as: :favorite_comics
   resources :comics, only: [:index] do
     member do
       patch 'favorite'
